2694d0704d45bf67b235c9ce825c68dc1009572e
Igor <igor.soaresbarbosa@hotmail.com>
2025-04-22 17:04:49 -0300

    debuging confidence interval and implementing pipeline in rwrw.py

--- rwre.py
+++ rwre.py
@@ -5,83 +5,50 @@ import time
 from tqdm import tqdm
 from scipy.stats import bernoulli
 
-
-
-
-
-'''Simple Symmetric Exclusion Process (SSEP)'''
-
-# The random environment is a SSEP
-
 def space_env(L , mu=0.5):
     total_space = np.arange(L)
     space = np.random.choice(total_space, int(L*mu), replace=False)  # choose L/2 of the bins to contain a particle
-    return(np.sort(space))                                           # return the index of the particles sorted
+    space = np.sort(space)
+    space = np.append(space,[0])
+    return(space)                                           # return the index of the particles sorted
 
+def search(space,L,old_idx=0): # This function checks if the position of rw(space[-1]) is at some particle
+    a = (space[-1])%L 
+    numbpart = len(space)-1
+    left,right = (old_idx%numbpart),((old_idx+1)%numbpart)
 
-def move_particle(space,s,L,mu=0.5):
-    #Moves s particles to a neighboring site if that new position is empty
-    particles_index = np.arange(len(space))
-    particles_to_move = np.random.choice(particles_index,s)
-
-    # Pick a random direction  for each step (left or right)
-    direction = np.random.choice([-1, 1], s)
+    if space[left]== a:
+        return True,left
+    if space[right]==a:
+        return True,left
     
-    # this is a torus, if the particle at L-1 jumps to right it goes to 0, and vice-versa
-    for idx_atual, dir_particle in zip(particles_to_move, direction):
-        # Check if the new position is empty (no particle present)
-        # Since we are in Z, the order of partiles is never changed, 
-        # therefore it's enough to check if new_postition is equal to position of next particle.
-        if ((space[idx_atual] + dir_particle)% L  ) != ((space[(idx_atual + dir_particle)%(int(L/2))])%L):
-            # Move the particle
-            space[idx_atual] = (space[idx_atual] + dir_particle)%L
+    if (space[left]> space[right]):  # in that case there is a jump between left and right
+        if a> space[left] or space[right]>a:
+            return False, left
 
-
-def search_in_rotated_array(space,a,L)-> bool: # This is an adaptation of binary search that decides wheter a belongs to space or not
-    a=a%L
-    left, right = 0, len(space) - 1 
-
+    # Determine a properly sorted interval [left, right]
+    while (space[left]> a)and(space[(left-1)%numbpart]<space[left]):
+        # first part checks if      a < left
+        # second part checks for a jump
+        left = (left - 1)%(numbpart)
+    while (space[right]<a)and(space[right]<space[(right+1)%numbpart]):
+        # first part checks if right < a
+        # second part checks for a jump
+        right = (right + 1)%(numbpart)    
+    # Binary search on [left,right]
     while left <= right:
-        mid = (left + right) // 2
-
+        mid = (left+right)//2
         if space[mid] == a:
-            return True  # Found the element
+            return True,left
+        
+        if space[mid]< a<=space[right]:
+            left = mid+1
+        elif space[left]<= a < space[mid]:
+            right = mid-1
+        else:
+            return False,left
+    return False,left
 
-        # Determine which side is properly sorted
-        if space[left] <= space[mid]:  # Left half is sorted
-            if space[left] <= a < space[mid]:  # Target in left half
-                right = mid - 1
-            else:  # Target in right half
-                left = mid + 1
-        else:  # Right half is sorted
-            if space[mid] < a <= space[right]:  # Target in right half
-                left = mid + 1
-            else:  # Target in left half
-                right = mid - 1
-
-    return False  # Element not found
-
-
-'''Random Walk'''
-
-
-def random_walk_step(space,rw,L,right_drift=1/3,left_drift=2/3):  
-    if search_in_rotated_array(space,rw,L): # in blue particles there is a right shift
-        direction = bernoulli.rvs(right_drift)  # sample 1 with prob 1/3 and 0 with probability 2/3 
-        direction = 2*direction -1              # change bernoulli(0,1) to bernoulli(-1,+1)
-        rw = rw + direction
-
-        return(rw)
-    
-    else :# in red particles there is a left shift
-        direction = bernoulli.rvs(left_drift)   # sample 1 with prob 2/3 and 0 with probability 1/3 
-        direction = 2*direction -1              # change bernoulli(0,1) to bernoulli(-1,+1)
-        rw = rw + direction
-
-        return(rw)
-
-
-def random_walk(L,t,right_drift=1/3,left_drift=2/3,mu=1/2,v=1): 
     # L means size of lattice,
     # v is the velocity of the environment particles, 
     # t is the number of steps of RW, 
@@ -99,16 +66,63 @@ def random_walk(L,t,right_drift=1/3,left_drift=2/3,mu=1/2,v=1):
         rw= random_walk_step(space,rw,L,right_drift,left_drift)
     return(rw)
 
+def pipeline_to_move(space,s,l_drift=1/3, r_drift=2/3,v=1):
+    # space is a vector with index of particles and the random walk in the last
+    # s is the number of steps taken by the random walk
+    numb_particles = len(space) -1                   # subtract one to not count rw as a particle
+    p_index = np.arange(numb_particles)      # enumerating the particles
+    p_steps = np.random.poisson(v*numb_particles,s)+1
+    total = np.cumsum(p_steps)              # determining the times whether random walks jumps
+    to_move = np.random.choice(p_index,total[-1])# determining which particle/rw jumps each time
+                
+    to_dir_l = np.random.choice([-1,1],total[-1])      # determining the vector of the directions to jump
+    to_dir_r = np.zeros(total[-1])                     # to avoid bugs
+    for n in total:                                    # random walk steps
+        to_dir_l[n-1] = 2*np.random.binomial(1,l_drift) -1 # sampling rw steps if there is a particle
+        to_dir_r[n-1] = 2*np.random.binomial(1,r_drift) -1 # sampling rw steps if there is not a particle
+        to_move[n-1]= numb_particles
+        
+    return to_move,to_dir_l,to_dir_r
 
-def generate_data(Numbsimul,L,t,right_drift,left_drift,mu=1/2,v=1):
+
+def can_particle_move(space, L, i, dir_l): # Boolean saying if the particle i can move dir_l
+    numb_particles = len(space)-1
+    actual_pos= space[i] 
+    new_pos = (actual_pos + dir_l)%L
+    next_part_idx = (i + dir_l)% numb_particles
+    next_part_pos = space[next_part_idx]
+
+    return new_pos != next_part_pos
+
+def move_all(space,L,to_move,to_dir_l,to_dir_r,old_part=0):
+    numb_particles = len(space)-1
+    for i,dir_l,dir_r in zip(to_move,to_dir_l,to_dir_r):
+        if i == numb_particles:                     # i is the rw to_move
+            rw_isinpart, old_part = search(space,L,old_part)
+            if rw_isinpart:        # rw is at a particle
+                space[-1] = space[-1] + dir_l
+            else:                                       # rw is not at a particle       
+                space[-1] = space[-1] + dir_r
+        else:                                       # i is a particle to move
+            if can_particle_move(space,L,i,dir_l):  # if particle i can move 
+                space[i] = (space[i] + dir_l)%L     # Move the particle
+
+def rw(L,s,mu=1/2,l_drift=1/3,r_drift=2/3,v=1):
+    space = space_env(L,mu)
+    to_move,to_dir_l,to_dir_r = pipeline_to_move(space,s,l_drift,r_drift,v)
+    move_all(space,L,to_move,to_dir_l,to_dir_r)
+    return space[-1] # returns the position of the random walk
+
+
+def generate_data(Numbsimul,L,t,mu=1/2,l_drift=1/3,r_drift=2/3,v=1):
     # L,v,t,mu  are exactly thte same parameters used in random_walk
     # N is the number of simulations made
 
-    data = np.array([random_walk(L,t,right_drift,left_drift,mu,v) for i in range(Numbsimul)])
+    data = np.array([rw(L,t,mu,l_drift,r_drift,v) for i in range(Numbsimul)])
     return(data)
 
 
-def full_data_random_walk(k1,k2,Numbsimul,right_drift=1/3,left_drift=2/3,mu=1/2,v=1):
+def full_data_random_walk(k1,k2,Numbsimul,mu=1/2,l_drift=1/3,r_drift=2/3,v=1):
 # k_1 is the minum size simulation and k2 is biggest 
     if k2<=k1:
         print('------------k2 must be bigger than k1-----------')
@@ -122,7 +136,7 @@ def full_data_random_walk(k1,k2,Numbsimul,right_drift=1/3,left_drift=2/3,mu=1/2,
 
     for i, k in tqdm(enumerate(range(k1, k2+1))):
         n = pow(2,k)
-        data[i,:] = np.array(generate_data(Numbsimul,n,n,right_drift,left_drift,mu,v))
+        data[i,:] = np.array(generate_data(Numbsimul,n,n+1,mu,l_drift,r_drift,v))
     print('\n')
     print("--- %s seconds ---" % (time.time() - start_time))
     print('\n')
